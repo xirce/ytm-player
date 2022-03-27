29f88336c783ed43e830e9b695f03aaf272a957e
DESKTOP-JIA02LP\Raph <abdyev_r@mail.ru>
2022-03-27 18:13:47 +0500

    links

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -4,13 +4,21 @@ import {AudioWrapper} from './AudioPlayer/AudioWrapper';
 import Box from "@mui/material/Box";
 import {ThemeProvider} from "@mui/material/styles";
 import mainTheme from '../mainTheme';
+import '../styles/Layout.css';
 
 export const Layout: React.FC = () => {
     return (
         <ThemeProvider theme={mainTheme}>
-                <Box sx={(theme) => ({backgroundColor: theme.palette.primary.dark})}>
-                    <Link to="/">Главная</Link>
-                    <Link to="/search">Поиск</Link>
+                <Box sx={(theme) => ({
+                    display: "flex",
+                    padding: "10px",
+                    alignItems: "center",
+                    gap: "20px",
+                    backgroundColor: theme.palette.primary.dark,
+                    height: "50px"
+                    })}>
+                    <Link to="/" className="navElem">Главная</Link>
+                    <Link to="/search" className="navElem">Поиск</Link>
                 </Box>
                 <Box sx={(theme) => ({
                     width: '100%',
